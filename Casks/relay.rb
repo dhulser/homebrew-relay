5f9c6a78bfef551b3c58a0c83b3db8a9c3ba7a1f
Dylan Hulser <dhulser@gmail.com>
2026-09-14 08:02:51 -0700

    Relay 1.1.0

--- Casks/relay.rb
+++ Casks/relay.rb
@@ -1,6 +1,6 @@
 cask "relay" do
-  version "1.0.0"
-  sha256 "78964eb2e761956f4196c837b5dc8737fe0e95521d2e5beb43e964feccc91fe2"
+  version "1.1.0"
+  sha256 "449b40d0034f9910b7ce0ea20ac77bf0f0600bfecc070f4b8232091d6fed3c37"
 
   url "https://github.com/dhulser/Relay/releases/download/v#{version}/Relay.dmg"
   name "Relay: Live Translator"
